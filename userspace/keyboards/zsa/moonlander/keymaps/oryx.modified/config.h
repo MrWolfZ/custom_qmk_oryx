2adc32ee26d7b1691a2c347f6a85e5cd2a95a854
Jonathan Ziller <jonathan.ziller@gmail.com>
2025-06-07 11:54:05 +0200

    ✨(oryx/MBodL/Qzqxap): Keyboard layout edited.

--- userspace/keyboards/zsa/moonlander/keymaps/oryx.modified/config.h
+++ userspace/keyboards/zsa/moonlander/keymaps/oryx.modified/config.h
@@ -5,7 +5,7 @@
 #define PERMISSIVE_HOLD
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "KyndX/VqaRyY"
+#define SERIAL_NUMBER "MBodL/Qzqxap"
 #define LAYER_STATE_8BIT
 
 #define RGB_MATRIX_STARTUP_SPD 60
